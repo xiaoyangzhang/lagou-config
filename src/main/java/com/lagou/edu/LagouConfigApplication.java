83960bfd443b586ba0f63f1c55992de61c950063
张晓阳 <zhangxy@guoguokeji.com.cn>
2020-09-14 23:03:31 +0800

    1

--- src/main/java/com/lagou/edu/LagouConfigApplication.java
+++ src/main/java/com/lagou/edu/LagouConfigApplication.java
@@ -2,8 +2,12 @@ package com.lagou.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
 public class LagouConfigApplication {
 
     public static void main(String[] args) {
